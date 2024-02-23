abf21c2b9019dab577f24b6e997bc8c8058d8190
asm-wizard <miguellaredobarbadillo@gmail.com>
2024-02-23 02:31:13 +0100

    torch imports

--- dnn/model.py
+++ dnn/model.py
@@ -1,6 +1,13 @@
 
-import numpy as np
 import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+import numpy as np
+
 
 
 
